eb00dff06cf92119612ea3a561b7e808820edab2
HiruniJAY <hirunijayalath74@gmail.com>
2026-03-04 23:51:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector(".container1 form").addEventListener("submit", function(e){
+    alert("Thank you for your feedback!");
+});
+
+document.querySelector(".form_container form").addEventListener("submit", function(e){
+    alert("Event saved!");
+});
